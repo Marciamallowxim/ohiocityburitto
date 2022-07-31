373c29a4a9e53eea7db4b9266991fc3a498cc057
Aladeen <aladeenuthy@gmail.com>
2022-07-31 21:10:38 +0100

    added hosting config

--- clone/settings.py
+++ clone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$ix((-$l0i1v(f!0)o5#53*#9o(gi6#gw=_tn_5pd#6w)6$or9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- clone/urls.py
+++ clone/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from ohio import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls, ),
